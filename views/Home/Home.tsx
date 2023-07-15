b402cdb420a2d484fc552bf70ba2e90e3a2d8c06
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-15 13:13:27 +0500

    left navigation completed

--- views/Home/Home.tsx
+++ views/Home/Home.tsx
@@ -1,20 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/home.module.scss";
 import { AiFillHome } from "react-icons/ai";
+import { BiSolidPlanet } from "react-icons/bi";
+import { BsFillCarFrontFill } from "react-icons/bs";
+import { IoIosPeople } from "react-icons/io";
+import { GiFilmSpool } from "react-icons/gi";
+import { GiSpectre } from "react-icons/gi";
+import Planets from "@/components/Planets/Planets";
 
 const Home = () => {
+  const [selectedButton, setSelectedButton] = useState<string | null>(null);
+
   return (
     <div className={styles.parent}>
       {/* left navigation */}
       <div className={styles.leftNavigation}>
-        <div className={styles.tab}>
-          {/* icon */}
-          <AiFillHome />
-          <p>Home</p>
-        </div>
+        {leftNavigationData.map((item, index) => {
+          return (
+            <button
+              onClick={() => setSelectedButton(item.name)}
+              key={index}
+              className={`${styles.tab} ${
+                selectedButton === item.name ? styles.selected : ""
+              }`}
+            >
+              {/* icon */}
+              {item.icon}
+              <p>{item.name}</p>
+            </button>
+          );
+        })}
+      </div>
+      {/* ===> right portion */}
+      <div className={styles.rightPortion}>
+        {selectedButton === "planets" && <Planets />}
       </div>
     </div>
   );
 };
+
+const leftNavigationData = [
+  {
+    icon: <AiFillHome />,
+    name: "home",
+  },
+  {
+    icon: <BiSolidPlanet />,
+    name: "planets",
+  },
+  {
+    icon: <BsFillCarFrontFill />,
+    name: "vehicles",
+  },
+  {
+    icon: <IoIosPeople />,
+    name: "people",
+  },
+  {
+    icon: <GiFilmSpool />,
+    name: "films",
+  },
+  {
+    icon: <GiSpectre />,
+    name: "species",
+  },
+];
 
 export default Home;
