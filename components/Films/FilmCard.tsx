cef3cb6ab450abd7a48f22a97097cea49a25db5e
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-17 03:09:49 +0500

    film section done with api integration and responsivenedd

--- components/Films/FilmCard.tsx
+++ components/Films/FilmCard.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import styles from "../../styles/films.module.scss";
 import Image from "next/image";
 
-const FilmCard = () => {
+interface Props {
+  name: string;
+  director: string;
+  producer: string;
+}
+
+const FilmCard = ({ name, director, producer }: Props) => {
   return (
     <div className={styles.filmCard}>
       <Image src="/assets/film.jpg" width={190} height={160} alt="" />
       <div className={styles.attributes}>
         {/* name */}
         <p className={styles.text}>
-          <span className={styles.textType}> title: </span> A New Hope
+          <span className={styles.textType}> title: </span> {name}
         </p>
         {/* climate */}
         <p className={styles.text}>
-          <span className={styles.textType}>director:</span> George Lucas
+          <span className={styles.textType}>director:</span> {director}
         </p>
         {/* population */}
         <p className={styles.text}>
-          <span className={styles.textType}>producer:</span> Gary Kurtz, Rick
-          McCallum Corporation
+          <span className={styles.textType}>producer:</span> {producer}
         </p>
       </div>
     </div>
--- components/Films/Films.tsx
+++ components/Films/Films.tsx
@@ -1,14 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/films.module.scss";
 import FilmCard from "./FilmCard";
+import Pagination from "../Pagination/Pagination";
+
+interface Film {
+  name: string;
+  director: string;
+  producer: string;
+}
 
 const Films = () => {
+  // states ------------------------------->
+  const [films, setFilms] = useState<Film[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filmsPerPage] = useState(3);
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  // here we are fetching the data from the api ------------------->
+  useEffect(() => {
+    setIsLoaded(true);
+    const fetchFilms = async () => {
+      try {
+        const response = await fetch("https://swapi.dev/api/films/");
+        const data = await response.json();
+        // Extracting only the required fields from each planet object --------------------->
+        const extractedPlanets = data.results.map((film: any) => ({
+          name: film.title,
+          director: film.director,
+          producer: film.producer,
+        }));
+
+        setFilms(extractedPlanets);
+        setIsLoaded(false);
+      } catch (error) {
+        console.log("Error fetching planets:🔥", error);
+      }
+    };
+
+    fetchFilms();
+  }, []);
+
+  // Calculate current planets to display based on pagination ------------------>
+  const indexOfLastFilm = currentPage * filmsPerPage;
+  const indexOfFirstFilm = indexOfLastFilm - filmsPerPage;
+  const currentFilms = films.slice(indexOfFirstFilm, indexOfLastFilm);
+
+  // Go to previous page ----------------------->
+  const goToPrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  // Go to next page ----------------->
+  const goToNextPage = () => {
+    const totalPages = Math.ceil(films.length / filmsPerPage);
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
     <div className={styles.filmParent}>
-      {/* ===> vehicles  */}
-      {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => {
-        return <FilmCard key={index} />;
-      })}
+      <div className={styles.cardsParent}>
+        {/* ===> vehicles  */}
+        {currentFilms.map((item, index) => {
+          return (
+            <FilmCard
+              name={item.name}
+              producer={item.producer}
+              director={item.director}
+              key={index}
+            />
+          );
+        })}
+      </div>
+      {/* Pagination -----------------------> */}
+      <Pagination
+        numOfCards={films}
+        cardsPerPage={filmsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        goToPrevPage={goToPrevPage}
+        goToNextPage={goToNextPage}
+      />
     </div>
   );
 };
