a05bfb61d6dc181f1ea8a46156db177d2dbeacb8
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-16 19:01:34 +0500

    planets section completed

---
+++ components/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {
+  totalCard: string;
+  cardsPerPage: string;
+}
+
+const Pagination = () => {
+  return <div>Pagination</div>;
+};
+
+export default Pagination;
