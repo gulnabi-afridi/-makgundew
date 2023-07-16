f716aae8fd78e0c55f3e15d1c1c0b9377341ef5e
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-16 22:13:30 +0500

    loader component developed

---
+++ components/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/loader.module.scss";
+
+const Loader = () => {
+  return (
+    <div className={styles.ldsRing}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
--- components/Planets/Planets.tsx
+++ components/Planets/Planets.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styles from "../../styles/planets.module.scss";
 import PlanetCard from "./PlanetCard";
 import { HiArrowLeft } from "react-icons/hi";
+import Pagination from "../Pagination/Pagination";
+import Loader from "../Loader/Loader";
 
 interface Planet {
   name: string;
@@ -11,18 +13,20 @@ interface Planet {
 }
 
 const Planets = () => {
-  // states ------------------------
+  // states ------------------------------->
   const [planets, setPlanets] = useState<Planet[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [planetsPerPage] = useState(3);
+  const [isLoaded, setIsLoaded] = useState(true);
 
-  // here we are fetching the data from the api --------------
+  // here we are fetching the data from the api ------------------->
   useEffect(() => {
+    setIsLoaded(true);
     const fetchPlanets = async () => {
       try {
         const response = await fetch("https://swapi.dev/api/planets/");
         const data = await response.json();
-        // Extracting only the required fields from each planet object
+        // Extracting only the required fields from each planet object --------------------->
         const extractedPlanets = data.results.map((planet: any) => ({
           name: planet.name,
           climate: planet.climate,
@@ -30,6 +34,7 @@ const Planets = () => {
         }));
 
         setPlanets(extractedPlanets);
+        setIsLoaded(false);
       } catch (error) {
         console.log("Error fetching planets:🔥", error);
       }
@@ -38,22 +43,19 @@ const Planets = () => {
     fetchPlanets();
   }, []);
 
-  // Calculate current planets to display based on pagination
+  // Calculate current planets to display based on pagination ------------------>
   const indexOfLastPlanet = currentPage * planetsPerPage;
   const indexOfFirstPlanet = indexOfLastPlanet - planetsPerPage;
   const currentPlanets = planets.slice(indexOfFirstPlanet, indexOfLastPlanet);
 
-  // Change page
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-
-  // Go to previous page
+  // Go to previous page ----------------------->
   const goToPrevPage = () => {
     if (currentPage > 1) {
       setCurrentPage((prevPage) => prevPage - 1);
     }
   };
 
-  // Go to next page
+  // Go to next page ----------------->
   const goToNextPage = () => {
     const totalPages = Math.ceil(planets.length / planetsPerPage);
     if (currentPage < totalPages) {
@@ -62,46 +64,38 @@ const Planets = () => {
   };
 
   return (
-    <div className={styles.planetsParent}>
-      <div className={styles.cardsParent}>
-        {/* ===> planet card */}
-        {currentPlanets.map((item, index) => {
-          return (
-            <PlanetCard
-              key={index}
-              name={item.name}
-              climate={item.climate}
-              population={item.population}
-            />
-          );
-        })}
-      </div>
-      {/* Pagination */}
-      <div className={styles.pagination}>
-        <button disabled={currentPage === 1} onClick={goToPrevPage}>
-          <HiArrowLeft className={styles.arrowLeft} />
-        </button>
-        {Array.from({ length: Math.ceil(planets.length / planetsPerPage) }).map(
-          (_, index) => (
-            <button
-              className={`${styles.btn} ${
-                currentPage === index + 1 ? styles.activeBtn : ""
-              }`}
-              key={index}
-              onClick={() => paginate(index + 1)}
-            >
-              {index + 1}
-            </button>
-          )
-        )}
-        <button
-          disabled={currentPage === Math.ceil(planets.length / planetsPerPage)}
-          onClick={goToNextPage}
-        >
-          <HiArrowLeft className={styles.arrowRight} />
-        </button>
-      </div>
-    </div>
+    <>
+      {isLoaded ? (
+        <div className={styles.loader}>
+          <Loader />
+        </div>
+      ) : (
+        <div className={styles.planetsParent}>
+          <div className={styles.cardsParent}>
+            {/*  planet card ---------------------> */}
+            {currentPlanets.map((item, index) => {
+              return (
+                <PlanetCard
+                  key={index}
+                  name={item.name}
+                  climate={item.climate}
+                  population={item.population}
+                />
+              );
+            })}
+          </div>
+          {/* Pagination -----------------------> */}
+          <Pagination
+            numOfCards={planets}
+            cardsPerPage={planetsPerPage}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            goToPrevPage={goToPrevPage}
+            goToNextPage={goToNextPage}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
